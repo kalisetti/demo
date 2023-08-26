8d6632e9bf338a88a048d123ccf2d9b3d92f7d47
kalisetti <sivasankar.k2003@gmail.com>
2023-08-26 20:37:29 +0600

    index updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function($){
+    "use strict";
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+})(jQuery);
